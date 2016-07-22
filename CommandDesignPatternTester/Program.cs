fe940fb0fe9674c3d523742f4f255f043f5ea866
falcopellegrino <franco.pellegrino@aruba.it>
2016-07-22 08:04:44 +0200

    primo commit

---
+++ CommandDesignPatternTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDesignPatternTester
+{
+
+    // Command
+    // Definition
+    // Encapsulate a request as an object, thereby letting you parameterize clients with different requests,
+    // queue or log requests, and support undoable operations. 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
